d1da2a751cc347e5418844dcea1a91f25db44b89
Infamous Hydra <147476468+Team-Infamous@users.noreply.github.com>
2025-04-27 12:23:22 +0545

    ㅤㅤ

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'solo-leveling-arise.vercel.app'],
   },
+  async beforeBuild() {
+    if (process.env.INIT_DB === 'true') {
+      const { initializeAdmin } = require('./lib/initDB');
+      await initializeAdmin();
+    }
+  }
 };
 
 module.exports = nextConfig;
